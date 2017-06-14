78d6e65d2a6e73b52dbdddd75596a2ea34d72001
Luni-4 <luni-4@hotmail.it>
2017-06-14 22:35:41 +0200

    Fix time measure

--- SVM.py
+++ SVM.py
@@ -1,6 +1,6 @@
 from sklearn.svm import SVC
 from sklearn.externals import joblib
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
 import numpy as np
 from dataload import load_adj, load_annotation 
 from utility import timer, memory
@@ -18,6 +18,10 @@ from utility import timer, memory
 # cv --> number of k-group used as train/test set
 # n_jobs --> if its value is -1, the work load is subdivided on the avaliable CPU
 
+# cross_val_predict parameters
+# http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_val_predict.html#sklearn.model_selection.cross_val_predict
+# It returns the prediction for each example
+
 
 if __name__ == '__main__':
 
@@ -29,18 +33,21 @@ if __name__ == '__main__':
     Y = memory(load_annotation)(filename1)
     
     # Define SVM Classifiers and save them in a list
-    c = [timer(SVC, x + 1)() for x in range(Y.shape[1])]
+    c = [timer(SVC, x + 1)(decision_function_shape = "ovr") for x in range(Y.shape[1])]
 
     # Run SVMs, print their execution time and save in a list the cross-validation scores
-    cv = [timer(cross_val_score, x + 1)(c[x], X, Y.getcol(x).data, cv = 5, n_jobs = -1) for x in range(Y.shape[1])]
+    y_pred = [timer(cross_val_predict, x + 1)(c[x], X, Y.getcol(x).data, cv = 5, n_jobs = -1, verbose = 5) for x in range(2)]   
     
     # Print cross-validation results for each classifier
-    for x in cv:
+    for x in y_pred:
         print(x)
+    
+    # Calculate the metrics    
+    metrics = [metrics(y_pred[x], Y.getcol(x).data) for x in range(2)]        
         
     # Save the models to disk
-    y = 1
+    '''y = 1
     for x in c:
        filename = "SVM_Classifiers/" + "Svm" + str(y) +".sav"
        joblib.dump(x, filename)
-       y += 1   
+       y += 1 '''  
---
+++ metrics.py
@@ -0,0 +1,10 @@
+# Recall --> http://scikit-learn.org/stable/modules/generated/sklearn.metrics.recall_score.html
+# Precision --> http://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_score.html
+# F-score --> http://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html
+
+# AUPRC ---> http://scikit-learn.org/stable/modules/generated/sklearn.metrics.average_precision_score.html
+# AUROC ---> http://scikit-learn.org/stable/modules/generated/sklearn.metrics.roc_auc_score.html#sklearn.metrics.roc_auc_score
+# Precision-Recall-F-score --> http://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_recall_fscore_support.html#sklearn.metrics.precision_recall_fscore_support
+
+def metrics(y_true, y_pred):
+   auprc = 
