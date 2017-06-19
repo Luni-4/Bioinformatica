d79979b0bcd47cc7e59295e7773ee5b9adf3d3a6
Federico Picetti <federico.picetti@gmail.com>
2017-06-19 14:10:08 +0200

    Add method to get column

--- dataload.py
+++ dataload.py
@@ -36,6 +36,7 @@ def load_annotation(filename):
                     data.append(v)
             nrows += 1
     m = sparse.csr_matrix((data, (row_ind, col_ind)), shape=(nrows, nclass))
+    m.getdensecol = lambda c:m.getcol(c).toarray().reshape(-1)
     return m  
 
     
@@ -54,7 +55,7 @@ def load_adj(filename, database="d", chunksize = 1024):
                 m = csr_vappend(m,sparse.csr_matrix(t, dtype=typ)) 
                 t = []
                 i = 0            
-        m = csr_vappend(m,sparse.csr_matrix(t, dtype=typ))              
+        m = csr_vappend(m,sparse.csr_matrix(t, dtype=typ))
     return m                    
                            
 
