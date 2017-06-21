621d9747ffa1976fab3db9bb62a8ba45144dd9ce
Federico Picetti <federico.picetti@gmail.com>
2017-06-21 14:30:02 +0200

    Now ada uses metrics

--- ada.py
+++ ada.py
@@ -3,26 +3,29 @@ from dataload import load_adj, load_annotation
 from sklearn.model_selection import cross_val_score
 from utility import timer
 from time import time
+from metrics import metrics
 
 def train_AdaBoost(X, Y):
     nclasses = Y.shape[1] # number of classes and classifiers
     classifiers = []
     for i in range (nclasses):
-        """
+        print('elaboro la classe {}'.format(i))
         y = Y.getdensecol(i)
         ada = AdaBoostClassifier()
         start = time()
-        ada.fit(X, y)
+        # ada.fit(X, y)
+        print(metrics(X, y, ada))
         end = time()
-        print(end - start)
-        classifiers.append(ada)
-        print(ada.score(X, y))
+        print('Ci messo {} secondi.'.format(end - start))
+        #classifiers.append(ada)
+        #print(ada.score(X, y))
         """
         start = time()
         est = cross_val_score(AdaBoostClassifier(), X, Y.getdensecol(i), cv=5, n_jobs = -1)
         end = time()
         print(end - start)
         print(est)
+        """
         
     
 
