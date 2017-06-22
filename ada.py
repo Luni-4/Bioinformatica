838a1e2f2888a090ce9cd603912bf3b08cd163b7
Federico Picetti <federico.picetti@gmail.com>
2017-06-22 15:43:58 +0200

    Try to create a scorer, but it should return a number, not a list

--- ada.py
+++ ada.py
@@ -4,12 +4,21 @@ from sklearn.model_selection import cross_val_score
 from utility import timer
 from time import time
 from metrics import metrics
+from sklearn.metrics import precision_score, recall_score, roc_auc_score, make_scorer
+
+prec_scorer = make_scorer(precision_score)
+rec_scorer = make_scorer(recall_score)
+auroc_scorer = make_scorer(roc_auc_score)
+def groupscorer(estimator, X, y):
+    return [prec_scorer(estimator, X, y), rec_scorer(estimator, X, y), auroc_scorer(estimator, X, y)]
+    
 
 def train_AdaBoost(X, Y):
     nclasses = Y.shape[1] # number of classes and classifiers
     classifiers = []
     for i in range (nclasses):
         print('elaboro la classe {}'.format(i))
+        """
         y = Y.getdensecol(i)
         ada = AdaBoostClassifier()
         start = time()
@@ -25,7 +34,7 @@ def train_AdaBoost(X, Y):
         end = time()
         print(end - start)
         print(est)
-        """
+        
         
     
 
