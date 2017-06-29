f47fd21514e49b5a1b04dc8219ea40560c32d90f
FedericoPicetti <federico.picetti@gmail.com>
2017-06-29 17:24:46 +0200

    Fixed comments and parameters

--- controller_secondo_federico.py
+++ controller_secondo_federico.py
@@ -1,26 +1,19 @@
 from sklearn.svm import SVC
 from sklearn.ensemble import AdaBoostClassifier
-#from myPegaso import Pegaso
+# from myPegaso import Pegaso
 from dataload import load_adj, load_annotation
 from metrics import metrics 
 from utility import timer, memory, debug, write_json
 
 import time
 import sys
-
-def SVM(f, o, X, Y, b = ""):
-
-    # For each classifier, calculate the required metrics 
-     
-    
+ 
 if __name__ == '__main__':
     
-    # Filename of the Adjacent Matrix
-    f_adj = "Data/Dros.adjmatrix.txt"
-    
     # Read Adjacent Matrix
-    X = load_adj(f_adj)
+    X = load_adj("Data/Dros.adjmatrix.txt")
     
+    # this structure could be more complicated, some parts can be commented out, we can use dicts to pass parameters
     classifiers = {
         'SVM_Balanced': SVC(decision_function_shape = "ovr", class_weight = "balanced"),
         # 'SVM_Balanced_C2' :SVC(decision_function_shape = "ovr", class_weight = "balanced", C=2)
@@ -46,14 +39,12 @@ if __name__ == '__main__':
         for classname, classifier in classifiers.items():
             filename_out = f_sim + classname + '.' + time.strftime("%c")'.json'
             # Save the header as a dictionary
-            header = [
-                        ("Ontology", onto_name),
+            header = [  ("Ontology", onto_name),
                         ("Classifier", classname),                  
                         ("Parameters", classifier.get_params()),                                    
                         ("Start_Time", time.strftime("%c"))
                      ]
-    
-    
+
             # Write the header into the json file 
             write_json(filename_out, header)
             for x in range(Y.shape[1]):
@@ -64,4 +55,3 @@ if __name__ == '__main__':
              
             # Write the footer into the json file 
             write_json(filename_out, footer)
-
