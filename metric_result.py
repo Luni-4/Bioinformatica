b363552464091135550404152e6899ec54e4cb43
Federico Picetti <federico.picetti@gmail.com>
2017-07-06 18:28:16 +0200

    add metric_on_population graph

--- metric_result.py
+++ metric_result.py
@@ -1,5 +1,6 @@
 import json
 import statistics
+import matplotlib.pyplot as plt
 
 class MetricResult:
 
@@ -30,18 +31,53 @@ class MetricResult:
     def stdevs(self, metric_name):
         """Returns a list of standard deviations for every class"""
         return [self.foldstdev(metric_name, c) for c in sorted(self.d['Data'].keys())]
+        
+    def metric_on_population_graph(self, metricname):
+        #TODO this should actually count positives in label matrix
+        populations = self.means('positives')
+        metrics = self.means(metricname)
+        plt.clf()
+        plt.scatter(populations, metrics)
+        plt.semilogx()
+        plt.xlabel("Positives")
+        plt.ylabel(metricname)
+        plt.show()
     
+def precision_recall_graph(precision, recall, auprc):
+    plt.clf() # clear the figure and don't close the graph window
+    plt.plot(precision, recall, linewidth = 2, color = "navy", label = "Precision-Recall Curve")
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title("Precision-Recall: AUPRC={0:0.2f}".format(auprc))
+    plt.legend(loc="lower right") # Position of the label defined in plt.plot
+    plt.savefig('prc.eps')
+    
+def roc_graph(fpr, tpr, auroc):
+    plt.clf()
+    plt.plot(fpr, tpr, linewidth = 2, color = "darkorange", label = "ROC Curve")
+    plt.plot([0, 1], [0, 1], linewidth = 2, linestyle = "--", color = "navy")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.title("ROC: AUROC={0:0.2f}".format(auroc))
+    plt.legend(loc="lower right")
+    plt.savefig('roc.eps')
+
+
+
 if __name__ == '__main__':
     fn = 'Simulation/CC/S_CC_SVM_Balanced.json'
     mr = MetricResult(fn)
     print(mr.foldmean('auroc', 234))
     print(mr.foldstdev('auroc', 234))
-    for foldno in range(5):
-        print(mr.d['Data'][234][foldno]['auroc'])
-    print(mr.means('auroc'))
-    print(mr.means('auroc')[234])
-    print(mr.stdevs('auroc'))
-    print(mr.stdevs('auroc')[234])
+    #print(mr.means('positives'))
+    mr.metric_on_population_graph('auroc')
+    
+    
+    
     """Return a dictionary, with keys:
     'End_Time', 'Data', 'Classifier', 'Parameters', 'Start_Time', 'Ontology'
 
